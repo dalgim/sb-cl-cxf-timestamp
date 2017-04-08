9b60e8f47d2d6a81dabbf89483eb1c928a5850be
dalgim <dalgiewiczmateusz@gmail.com>
2017-04-08 15:04:39 +0200

    Initial commit

---
+++ src/main/java/com/dalgim/example/sb/cxf/SbClCxfWsSslP12ProducerApplication.java
@@ -0,0 +1,12 @@
+package com.dalgim.example.sb.cxf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbClCxfWsSslP12ProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbClCxfWsSslP12ProducerApplication.class, args);
+	}
+}
